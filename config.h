7e1732ceb3f3ac58927ca74325813e996056a0a0
Ioan Nicu <ioan.nicu@gmail.com>
2020-05-09 15:36:07 +0200

    Apply dwm-restartsig-20180523-6.2.diff
    https://dwm.suckless.org/patches/restartsig/
    
    Signed-off-by: Ioan Nicu <ioan.nicu@gmail.com>

--- config.h
+++ config.h
@@ -1,5 +1,7 @@
 /* See LICENSE file for copyright and license details. */
 
+#include <X11/XF86keysym.h>
+
 /* appearance */
 static const unsigned int borderpx  = 1;        /* border pixel of windows */
 static const unsigned int snap      = 32;       /* snap pixel */
@@ -11,7 +13,8 @@ static const char col_gray1[]       = "#222222";
 static const char col_gray2[]       = "#444444";
 static const char col_gray3[]       = "#bbbbbb";
 static const char col_gray4[]       = "#eeeeee";
-static const char col_cyan[]        = "#005577";
+//static const char col_cyan[]        = "#005577";
+static const char col_cyan[]        = "#0034af";
 static const char *colors[][3]      = {
 	/*               fg         bg         border   */
 	[SchemeNorm] = { col_gray3, col_gray1, col_gray2 },
@@ -56,23 +59,40 @@ static const Layout layouts[] = {
 
 /* commands */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
+#define TERM "urxvt"
 static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
-static const char *termcmd[]  = { "st", NULL };
+static const char *termcmd[]  = { TERM, NULL };
+static const char *calcmd[] = { "zenity", "--calendar", NULL };
+static const char *xplorercmd[] = { TERM, "-e", "ranger", NULL };
+static const char *volupcmd[] = { "pactl", "--", "set-sink-volume", "@DEFAULT_SINK@", "+5%", NULL};
+static const char *voldowncmd[] = { "pactl", "--", "set-sink-volume", "@DEFAULT_SINK@", "-5%", NULL};
+static const char *volmutecmd[] = { "pactl", "set-sink-mute", "@DEFAULT_SINK@", "toggle", NULL};
+static const char *backlightupcmd[] = { "xbacklight", "-inc", "5", NULL};
+static const char *backlightdowncmd[] = { "xbacklight", "-dec", "5", NULL};
 
 static Key keys[] = {
 	/* modifier                     key        function        argument */
-	{ MODKEY,                       XK_p,      spawn,          {.v = dmenucmd } },
-	{ MODKEY|ShiftMask,             XK_Return, spawn,          {.v = termcmd } },
+	{ MODKEY,                       XK_d,      spawn,          {.v = dmenucmd } },
+	{ MODKEY,                       XK_Return, spawn,          {.v = termcmd } },
+	{ MODKEY,                       XK_c,      spawn,          {.v = calcmd } },
+	{ MODKEY,                       XK_x,      spawn,          {.v = xplorercmd } },
+	{ 0,         XF86XK_AudioRaiseVolume,      spawn,          {.v = volupcmd } },
+	{ 0,         XF86XK_AudioLowerVolume,      spawn,          {.v = voldowncmd } },
+	{ 0,                XF86XK_AudioMute,      spawn,          {.v = volmutecmd } },
+	{ 0,          XF86XK_MonBrightnessUp,      spawn,          {.v = backlightupcmd } },
+	{ 0,        XF86XK_MonBrightnessDown,      spawn,          {.v = backlightdowncmd } },
 	{ MODKEY,                       XK_b,      togglebar,      {0} },
-	{ MODKEY,                       XK_j,      focusstack,     {.i = +1 } },
-	{ MODKEY,                       XK_k,      focusstack,     {.i = -1 } },
-	{ MODKEY,                       XK_i,      incnmaster,     {.i = +1 } },
-	{ MODKEY,                       XK_d,      incnmaster,     {.i = -1 } },
-	{ MODKEY,                       XK_h,      setmfact,       {.f = -0.05} },
-	{ MODKEY,                       XK_l,      setmfact,       {.f = +0.05} },
-	{ MODKEY,                       XK_Return, zoom,           {0} },
+	{ MODKEY,                       XK_k,      focusstack,     {.i = +1 } }, // Next in stack
+	{ MODKEY,                       XK_j,      focusstack,     {.i = -1 } }, // Prev in stack
+	{ MODKEY,                       XK_Right,  focusstack,     {.i = +1 } }, // Next in stack
+	{ MODKEY,                       XK_Left,   focusstack,     {.i = -1 } }, // Prev in stack
+	{ MODKEY,                       XK_i,      incnmaster,     {.i = +1 } }, // Increase number of items in master
+	{ MODKEY,                       XK_p,      incnmaster,     {.i = -1 } }, // Decrease number of items in master
+	{ MODKEY,                       XK_h,      setmfact,       {.f = -0.05} }, // Decrease master area
+	{ MODKEY,                       XK_l,      setmfact,       {.f = +0.05} }, // Increase master area
+	{ MODKEY|ShiftMask,             XK_Return, zoom,           {0} },          // Make current the master
 	{ MODKEY,                       XK_Tab,    view,           {0} },
-	{ MODKEY|ShiftMask,             XK_c,      killclient,     {0} },
+	{ MODKEY|ShiftMask,             XK_q,      killclient,     {0} },
 	{ MODKEY,                       XK_t,      setlayout,      {.v = &layouts[0]} },
 	{ MODKEY,                       XK_f,      setlayout,      {.v = &layouts[1]} },
 	{ MODKEY,                       XK_m,      setlayout,      {.v = &layouts[2]} },
@@ -80,10 +100,10 @@ static Key keys[] = {
 	{ MODKEY|ShiftMask,             XK_space,  togglefloating, {0} },
 	{ MODKEY,                       XK_0,      view,           {.ui = ~0 } },
 	{ MODKEY|ShiftMask,             XK_0,      tag,            {.ui = ~0 } },
-	{ MODKEY,                       XK_comma,  focusmon,       {.i = -1 } },
-	{ MODKEY,                       XK_period, focusmon,       {.i = +1 } },
-	{ MODKEY|ShiftMask,             XK_comma,  tagmon,         {.i = -1 } },
-	{ MODKEY|ShiftMask,             XK_period, tagmon,         {.i = +1 } },
+	{ MODKEY,                       XK_comma,  focusmon,       {.i = -1 } }, // Focus prev monitor
+	{ MODKEY,                       XK_period, focusmon,       {.i = +1 } }, // Focus next monitor
+	{ MODKEY|ShiftMask,             XK_comma,  tagmon,         {.i = -1 } }, // Move focused window to prev monitor
+	{ MODKEY|ShiftMask,             XK_period, tagmon,         {.i = +1 } }, // Move focused window to next monitor
 	TAGKEYS(                        XK_1,                      0)
 	TAGKEYS(                        XK_2,                      1)
 	TAGKEYS(                        XK_3,                      2)
@@ -93,8 +113,8 @@ static Key keys[] = {
 	TAGKEYS(                        XK_7,                      6)
 	TAGKEYS(                        XK_8,                      7)
 	TAGKEYS(                        XK_9,                      8)
-	{ MODKEY|ShiftMask,             XK_q,      quit,           {0} },
-	{ MODKEY|ControlMask|ShiftMask, XK_q,      quit,           {1} }, 
+	{ MODKEY|ShiftMask,             XK_x,      quit,           {0} },
+	{ MODKEY|ShiftMask,             XK_r,      quit,           {1} },
 };
 
 /* button definitions */
